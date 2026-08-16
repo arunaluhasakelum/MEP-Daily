ff758ade9a98515085ffab11b7ada3cc58288aa6
arunaluhasakelum <73768011+arunaluhasakelum@users.noreply.github.com>
2026-08-16 09:32:58 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = 'mep-all-shell-v1';
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(['./index.html'])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (e) => { self.clients.claim(); });
+self.addEventListener('fetch', (e) => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(fetch(e.request).catch(() => caches.match('./index.html')));
+  }
+});
